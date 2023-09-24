15448a73180229e74e9ba18bc9f7935b4f8c3fb6
Yusupov Elbek <elbeky829@gmail.com>
2023-09-24 20:40:40 +0500

    modal added

---
+++ contact-app/src/components/contactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './ContactList.scss'
+
+const ContactList = () => {
+  return (
+    <div className="list">
+        <div className="container">
+            <div className="list__wrapper">
+                <div className="list__parent">
+                    <div className="list__child">
+                        <h2>First name: </h2>
+                        <h2>Last name: </h2>
+                        <h2>Phone: </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactList
